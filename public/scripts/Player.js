97958d94245798a7d9e533270544a36b6be0bf03
James Sherry <james.m.sherry@googlemail.com>
2020-12-08 23:12:49 +0000

    added player class

---
+++ public/scripts/Player.js
@@ -0,0 +1,22 @@
+import { uuidv4 } from './utilities.js';
+import BirthChart from './BirthChart.js'
+
+class Player {
+  constructor({ name, birthchart, _id=uuidv4() }) {
+    this._id = _id;
+
+    if(typeof name !== 'string' ) {
+      throw new Error(`Player name must be a string; instead received ${name} (type: ${typeof name})`);
+    } else if (typeof name === 'string' && name.length < 3) {
+      throw new Error(`Player name must be at least 3 characters long; received ${name} (length: ${name.length})`);
+    }
+
+    this.name = name;
+
+    if(!(birthchart instanceof BirthChart)) {
+      throw new Error(`birthchart name must be a birthcart; instead received ${birthchart} (type: ${birthchart.__proto__.constructor})`);
+    }
+  }
+}
+
+export default Player;
