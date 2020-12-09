9570762413ccb238d9891c9ab0d96f0976102b3f
James Sherry <james.m.sherry@googlemail.com>
2020-12-09 06:06:37 +0000

    my bad; added clickable addresses for prod launch

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ import app from './server.js';
 
 const {
   PORT=3000,
+  NODE_ENV,
 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
+  if(NODE_ENV === 'production') {
+    console.log(`Birthchart List page: http://localhost:${PORT}/`);
+    console.log(`Add Birthchart page:  http://localhost:${PORT}/add-birth-data.html`);
+    console.log(`Calling Page:  http://localhost:${PORT}/generator.html`);
+    console.log(`Public grid page:  http://localhost:${PORT}/call-reporter.html`);
+    console.log(`Example Chart page:  http://localhost:${PORT}/example-chart.html`);
+  }
 });
