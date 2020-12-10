3f1c57d42f9c22aa19822a9f2ccf8e3026fe26a4
James Sherry <james.m.sherry@googlemail.com>
2020-12-10 04:51:14 +0000

    fixed display urls

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ const {
   NODE_ENV,
 } = process.env;
 
+const isProd = NODE_ENV === 'production';
+
 // Read users.json file 
 const config = JSON.parse(fs.readFileSync('./.vscode/settings.json', { encoding: 'utf8'}));
 const DEV_SERVER_PORT = config['liveServer.settings.port'];
 
+const WEB_HOST_PORT = isProd ? PORT : DEV_SERVER_PORT
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
   // if(NODE_ENV === 'production') {
-    console.log(`Birthchart List page: http://localhost:${DEV_SERVER_PORT}/`);
-    console.log(`Add Birthchart page:  http://localhost:${DEV_SERVER_PORT}/add-birth-data.html`);
-    console.log(`Calling Page:  http://localhost:${DEV_SERVER_PORT}/generator.html`);
-    console.log(`Public grid page:  http://localhost:${DEV_SERVER_PORT}/call-reporter.html`);
-    console.log(`Example Chart page:  http://localhost:${DEV_SERVER_PORT}/example-chart.html`);
+    console.log(`Birthchart List page: http://localhost:${WEB_HOST_PORT}/`);
+    console.log(`Add Birthchart page:  http://localhost:${WEB_HOST_PORT}/add-birth-data.html`);
+    console.log(`Calling Page:  http://localhost:${WEB_HOST_PORT}/generator.html`);
+    console.log(`Public grid page:  http://localhost:${WEB_HOST_PORT}/call-reporter.html`);
+    console.log(`Example Chart page:  http://localhost:${WEB_HOST_PORT}/example-chart.html`);
   // }
 });
